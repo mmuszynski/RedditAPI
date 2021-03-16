70151176006662dff7e876490925b669c4206296
Mike Muszynski <mmuszynski@gmail.com>
2021-03-16 11:17:03 -0400

    adding docs

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # RedditAPI
 
 A Swift Package that connects to the [Reddit API](https://www.reddit.com/dev/api), currently allowing read-only access to subreddit and user information.
---
-The following classes are implemented
+
+The following classes are implemented:
 
 ## Listing
+The basic structure returned by a call to the Reddit API. Each listing consists of a number of `Thing`s, which may represent various different objects (see `Thing.ThingType` for more detail).
+
+Conforms to 
+
+##
+
--- Sources/RedditAPI/Listing.swift
+++ Sources/RedditAPI/Listing.swift
@@ -8,20 +8,41 @@
 
 import Foundation
 
+/// The basic structure returned by Reddit for pagination and filtering.
+///
+/// Conforms to `Codable` in order to allow for decoding from JSON values returned by the API.
+///
+/// According to the [Reddit API Documentation](https://www.reddit.com/dev/api/#listings):
+///
+/// Many endpoints on reddit use the same protocol for controlling pagination and filtering. These endpoints are called Listings and share five common parameters: after / before, limit, count, and show.
+///
+/// Listings do not use page numbers because their content changes so frequently. Instead, they allow you to view slices of the underlying data. Listing JSON responses contain after and before fields which are equivalent to the "next" and "prev" buttons on the site and in combination with count can be used to page through the listing.
 public struct Listing: Codable {
-    public typealias Element = Thing
-        
+    
+    /// A string the represents the kind of data returned by the API.
+    ///
+    /// - note: For `Listing` objects, this should return "Listing"
     public var kind: String
+    
+    /// The various data elements returned by the `Listing` JSON
     private var data: ListingData
     
-    public var galleryImageURLs: [URL] {
-        var urls = [URL]()
-        for thing in self {
-            urls.append(contentsOf: thing.galleryImageURLs)
-        }
-        return urls
-    }
-    
+//    public var galleryImageURLs: [URL] {
+//        var urls = [URL]()
+//        for thing in self {
+//            urls.append(contentsOf: thing.galleryImageURLs)
+//        }
+//        return urls
+//    }
+  
+    /// A URL to provide the "next" slice of content from the API
+    ///
+    /// Reddit `Listing`s provide a snapshot of the site at an given time. Becuase content changes rapidly, the next slice is calculated from a given `Thing` and link. A current link is required to provide sorting and filtering information (e.g. hot or controversial posts for a given subreddit), and a suitable "next" page is created from the "after" element in the `ListingData`.
+    ///
+    /// If there is no suitable link after the current slice, this method will return `nil`.
+    ///
+    /// - Parameter current: The link used to request this `Listing` slice
+    /// - Returns: A `URL` that will return a `Listing` representing the next page or `nil` if no such page exists (such as when a subreddit's current posts have been exhasuted)
     public func nextURL(after current: URL) -> URL? {
         guard let after = data.after, var components = URLComponents(string: current.absoluteString) else {
             return nil
@@ -36,6 +57,7 @@ public struct Listing: Codable {
 }
 
 extension Listing: RandomAccessCollection {
+    public typealias Element = Thing
     public typealias Index = Array<Element>.Index
 
     private var elements: [Element] {
